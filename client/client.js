4c944b78dfdaafe7596e6722320f71720be6627e
Alexander Georgiev <donsandini@gmail.com>
2017-01-29 23:39:41 +0200

    css support added in webpack config

--- client/client.js
+++ client/client.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import createStore from './js/redux/createStore';
 import { Root } from './js/containers';
+require('./css/main.css');
 
 const store = createStore();
 const rootElement = document.getElementById('app');
